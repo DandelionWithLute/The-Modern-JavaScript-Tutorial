59780b1bf84c37de403152921e94707456711fe2
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-05-02 20:45:26 +0800

    main

--- 5 Data types/Array methods Tasks.js
+++ 5 Data types/Array methods Tasks.js
@@ -1,1 +1,10 @@
 // https://javascript.info/array-methods#tasks
+console.log("TASK1");
+console.log("Translate border-left-width to borderLeftWidth:");
+let notYetCamelizedStr = "border-left-width";
+let itsSaidToBeArr = notYetCamelizedStr.split("-");
+console.log(itsSaidToBeArr);
+let res = itsSaidToBeArr.map((word, index) =>
+  index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+).join("");
+console.log(res)
