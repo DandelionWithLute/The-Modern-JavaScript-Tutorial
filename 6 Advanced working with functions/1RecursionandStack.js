b3eb162c3697be9675b8f87f55738a98cd369f76
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-05-08 10:29:56 -0400

    main

--- 6 Advanced working with functions/1RecursionandStack.js
+++ 6 Advanced working with functions/1RecursionandStack.js
@@ -1,2 +1,13 @@
 // console.log(pow(2,3))
 // No such function, you need to write it by yourself.
+
+let tasks = `Sum all numbers till the given one
+Calculate factorial
+Fibonacci numbers
+Output a single-linked list
+Output a single-linked list in the reverse order 
+    Recursion: if (list.next) {printReverseList(list.next);}
+    Iterative: arr
+`;
+
+console.log(tasks);
---
+++ 6 Advanced working with functions/2RestParametersAndSpreadSyntax.js
@@ -0,0 +1,27 @@
+console.log("Usage of args");
+// Important: I didn't know this before.
+function sumAll(...args) {
+  // args is the name for the array
+  let sum = 0;
+  for (let arg of args) {
+    sum += arg;
+  }
+  console.log(args);
+
+  return sum;
+}
+
+console.log(sumAll(1, 2, 5, 87, 2));
+console.log();
+///////////////////////////////////////////////////
+console.log("Max comparison including arr");
+let compareMaxArr1 = [3, 77, 21, 2];
+let compareMaxArr2 = [66, 2, 1, 34, 77];
+console.log(Math.max(2, 55, 12, ...compareMaxArr1, ...compareMaxArr2));
+console.log();
+///////////////////////////////////////////////////
+console.log("arrToMerge & arrToCopy");
+let arrToMerge = [...compareMaxArr1, ...compareMaxArr2];
+console.log(arrToMerge);
+console.log("This way is much shorter and easier than Object.assign({},copy)");
+console.log();
