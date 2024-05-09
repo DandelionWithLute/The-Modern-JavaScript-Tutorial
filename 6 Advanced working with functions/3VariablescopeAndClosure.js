91d91bb55b47f014a79ffef6418bd6eacb7b8e88
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-05-09 11:11:06 -0400

    main

--- 6 Advanced working with functions/3VariablescopeAndClosure.js
+++ 6 Advanced working with functions/3VariablescopeAndClosure.js
@@ -13,12 +13,45 @@ const makeCounter = () => {
   };
 };
 
-let counter = makeCounter();
-console.log(counter);
-console.log(counter());
-console.log(counter());
-console.log(counter());
+let counterZero = makeCounter();
+console.log(counterZero);
+console.log(counterZero());
+console.log(counterZero());
+console.log(counterZero());
 
 // So, counter.[[Environment]] has the reference to {count: 0} Lexical Environment.
 //  That’s how the function remembers where it was created, no matter where it’s called.
 //  The [[Environment]] reference is set once and forever at function creation time.
+
+console.log("Tasks:");
+console.log("Does a function pickup latest changes?");
+console.log("Which variables are available?");
+console.log("IDK1:Are counters independent?");
+
+function makeCounterSameFunc() {
+  let count = 0;
+
+  return function () {
+    return count++;
+  };
+}
+
+let counter = makeCounterSameFunc();
+let counter2 = makeCounterSameFunc();
+
+console.log(counter()); // 0
+console.log(counter()); // 1
+
+console.log(counter2()); // ?
+console.log(counter2()); // ?
+
+// Important
+console.log("IDK2:Sum with closures");
+const sum = (a) => {
+  return function (b) {
+    return a + b;
+  };
+};
+
+console.log(sum(1)(-2));
+console.log(sum(432)(21321));
