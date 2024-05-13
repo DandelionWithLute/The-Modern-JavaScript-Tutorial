33828f5377ebbcabf5f18dd38232a85af6bffa30
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-05-13 13:23:37 -0400

    main

---
+++ 8 Prototypes Inheritance/4PrototypeMethodsObjsWithout__proto__.js
@@ -0,0 +1,64 @@
+let obj = {
+  animal: "dog",
+  sound: "bark",
+  sex: "male",
+};
+
+console.log("Object.getPrototypeOf(obj):\n" + Object.getPrototypeOf(obj));
+Object.defineProperty(obj, "animal", { writable: false });
+obj.animal = "cat";
+console.log();
+console.log(
+  `After Object.defineProperty(obj, "animal", { writable: false });\nThe animal is:` +
+    obj.animal
+);
+console.log();
+console.log("Object.getPrototypeOf(obj):\n" + Object.getPrototypeOf(obj));
+console.log();
+
+let animal = {
+  eats: true,
+};
+
+let rabbit = Object.create(animal);
+console.log("Object.create(obj) is the same as {__proto__:obj}");
+console.log(rabbit.eats);
+
+let desc1 = `
+The Object.create method is a bit more powerful,
+ as it has an optional second argument: property descriptors.`;
+console.log(desc1);
+
+// Intellisense:Creates an object that has the specified prototype,
+//  and that optionally contains specified properties.
+let rabbit2 = Object.create(animal, {
+  jumps: {
+    value: true,
+  },
+});
+console.log(
+  "I used to doubt the difference between Object.assign()and Object.create() method."
+);
+
+console.log(rabbit2.jumps);
+// Don’t change [[Prototype]] on existing objects if speed matters
+
+// "Very plain" objects
+// an interesting glitch: all keys work fine except "__proto__"
+// https://javascript.info/prototype-methods
+
+let chineseDict = Object.create(null);
+chineseDict.hello = "你好";
+chineseDict.bye = "再见";
+console.log(chineseDict);
+
+// Object.defineProperties()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperties
+
+let summaryDesc = `
+    To create an obj:
+        literal:{__proto__:...}
+        Object.create(Object.getPrototypeOf(obj), Object.getOwnPropertyDescriptors(obj))
+        Easy to Memory Verision: Object.create(Prototype,OPDescriptors)
+`;
+console.log(summaryDesc);
--- 8 Prototypes Inheritance/readme.md
+++ 8 Prototypes Inheritance/readme.md
@@ -2,3 +2,5 @@ My notes:
 \_\_proto\_\_ is mainly related to objects,
 while prototype is mainly related to functions,
 vise versa for constructor.
+
+dot prototype is the advanced and modern replacement of \_\_proto\_\_
