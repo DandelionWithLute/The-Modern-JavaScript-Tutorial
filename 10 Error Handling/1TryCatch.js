f343c40f1fbc8126db7c43b318e3ee7e72446bb7
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-05-17 09:24:14 -0400

    main

---
+++ 10 Error Handling/1TryCatch.js
@@ -0,0 +1,12 @@
+try {
+  console.log("Start Trying...");
+  lsalsa;
+} catch (err) {
+  console.log("Error occurred...");
+  console.log(err);
+}
+
+
+// try...catch only works for runtime errors
+// the code must be runnable
+// it should be valid JavaScript
