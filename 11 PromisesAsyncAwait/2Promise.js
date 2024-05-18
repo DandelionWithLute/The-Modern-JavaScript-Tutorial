25a023ac54625d03ccf065dc4d89cbf18d3610c7
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-05-18 04:49:15 -0400

    main

--- 11 PromisesAsyncAwait/2Promise.js
+++ 11 PromisesAsyncAwait/2Promise.js
@@ -8,17 +8,23 @@
 
 let promise = new Promise(function (resolve, reject) {
   // executor (the producing code, "singer")
+
+  // if the job is finished successfully, with result value.
+  resolve(value);
+  // if an error has occurred, error is the error object.
+  reject(error);
 });
-
-// if the job is finished successfully, with result value.
-resolve(value);
-// if an error has occurred, error is the error object.
-reject(error);
-
 // There can be only a single result or an error
 let promise2 = new Promise(function (resolve, reject) {
   resolve("done");
 
   reject(new Error("…")); // ignored
   setTimeout(() => resolve("…")); // ignored
+});
+
+// Reject with Error Objects
+
+// Immediately calling resolve/reject
+let promise3 = new Promise((resolve, reject) => {
+  resolve(123);
 });
