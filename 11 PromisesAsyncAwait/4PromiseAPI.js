02f1c46027b5440a1f3d37dbf0a63ca2a06afcc1
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-05-19 15:18:35 -0400

    main

--- 11 PromisesAsyncAwait/4PromiseAPI.js
+++ 11 PromisesAsyncAwait/4PromiseAPI.js
@@ -7,19 +7,37 @@ Promise.all([
   new Promise((resolve) => setTimeout(() => resolve(3), 3000)),
 ]).then(console.log);
 
-let names = ["mojombo", "defunkt", "pjhyett", "wycats"];
+let names = ["iliakan", "remy", "jeresig"];
 
 let requests = names.map((name) => {
   fetch(`http://api.github.com/users/${name}`);
 });
 
-Promise.all(requests)
-  .then((responses) => {
-    for (let res of responses) {
-      console.log(`${res.url}:${res.status}`);
-    }
-    return responses;
-  })
-  .then((responses) => Promise.all(responses.map((r) => r.json())))
-  // all JSON answers are parsed: "users" is the array of them
-  .then((users) => users.forEach((user) => alert(user.name)));
+Promise.all(requests).then(console.log(requests));
+
+// If any of the promises is rejected,
+//  the promise returned by Promise.all immediately rejects with that error.
+{
+  //     Promise.all([
+  //   new Promise((resolve, reject) => setTimeout(() => resolve(1), 1000)),
+  //   new Promise((resolve, reject) =>
+  //     setTimeout(() => reject(new Error("Whoops!")), 2000)
+  //   ),
+  //   new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000)),
+  // ]).catch(alert); // Error: Whoops!
+}
+// Promise.all(iterable) allows non-promise “regular” values in iterable
+
+// Can't use {} after => because resolve is not a func.
+console.log("\nPromise.all here!")
+// Promise.all([new Promise((resolve,reject)=>{setTimeout(()=>{resolve(1),1000})})]);
+Promise.all([new Promise((resolve,reject)=>{setTimeout(()=>{resolve(1),1000})})]);
+
+let summary = `
+1.Promise.all(promises)-wait for all promises to resolve and returns an array of their results.
+2.Promise.allSettled(promises) // Recently added
+3.race method // but waits only for the first settled promise and gets its result (or error).
+4.any method // Recently added
+5.Promise.resolve(value) – makes a resolved promise with the given value.
+6.Promise.reject(error) – makes a rejected promise with the given error.
+`
