069d40f7a9752dedbb1a641bdb848c564a7009ac
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-05-21 08:11:08 -0400

    main

---
+++ 12 Generators&AdvancedIterations/2AsyncIterationAndGenerators.js
@@ -0,0 +1,17 @@
+// Important
+// Async generators (finally)
+async function* generateSequence(start, end) {
+  for (let i = start; i <= end; i++) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    yield i;
+  }
+}
+
+(async () => {
+  let generator = generateSequence(1, 5);
+  for await (let value of generator) {
+    console.log(value);
+  }
+})();
+
+// Real-life example: paginated data
