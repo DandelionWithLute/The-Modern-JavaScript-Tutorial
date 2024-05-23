bcf19b49e16cb91e68eccceeff3132177970f903
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-05-23 13:31:55 -0400

    main

--- 14 Miscellaneous/1ProxyandReflect.js
+++ 14 Miscellaneous/1ProxyandReflect.js
@@ -92,14 +92,14 @@ console.log(array2[-2]);
 console.log();
 console.log("Task3:");
 
-let handlers = Symbol('handlers');
+let handlers = Symbol("handlers");
 
 function makeObservable(target) {
   // 1. Initialize handlers store
   target[handlers] = [];
 
   // Store the handler function in array for future calls
-  target.observe = function(handler) {
+  target.observe = function (handler) {
     this[handlers].push(handler);
   };
 
@@ -107,12 +107,13 @@ function makeObservable(target) {
   return new Proxy(target, {
     set(target, property, value, receiver) {
       let success = Reflect.set(...arguments); // forward the operation to object
-      if (success) { // if there were no error while setting the property
+      if (success) {
+        // if there were no error while setting the property
         // call all handlers
-        target[handlers].forEach(handler => handler(property, value));
+        target[handlers].forEach((handler) => handler(property, value));
       }
       return success;
-    }
+    },
   });
 }
 
@@ -125,3 +126,9 @@ user3.observe((key, value) => {
 });
 
 user3.name = "John";
+
+let symbolVariable = Symbol.for("anExample");
+console.log(symbolVariable);
+let anotherSV = Symbol.for("anExample");
+console.log(anotherSV);
+console.log(symbolVariable === anotherSV);
